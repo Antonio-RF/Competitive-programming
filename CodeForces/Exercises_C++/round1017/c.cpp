ae95b1b7d1413ba03eaef648ad6bce4a0572cebe
ANTONIO DA RESSURREICAO FILHO <arf24@inf.ufpr.br>
2025-04-30 11:39:05 -0300

    Add round 1020

---
+++ CodeForces/Exercises_C++/round1017/c.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+	int t;
+	cin >> t;
+	while(t--) {
+		int n;
+		cin >> n;
+		int tam = 2 * n;
+		int vector[tam];
+		map <int, int> v;
+
+		int input;
+		for (int i=0 ; i<n ; i++) {
+			for (int j=0 ; j<n ; j++) {
+				cin >> input;
+				if (i==0) {
+					vector[i+j+1] = input;
+					v[input] = 1;
+				}
+				if (i!=0 && j==(n-1)) {
+					vector[i+j+1] = input;
+					v[input] = 1;
+				}
+			}
+		}
+		for (int i=1 ; i<tam+1 ; i++) {
+			if (v[i] == 0) {
+				vector[0] = i;
+				break;
+			}
+		}
+		for (int k=0 ; k<tam ; k++) {
+			cout << vector[k] << " ";
+		}
+		cout << endl;
+	}
+    return 0;
+}
+
+
