fcfb67a54c93f52e67061e5ea073233c3073e70b
Antonio-RF <antoniidaressurreicao@gmail.com>
2026-01-21 09:42:21 -0300

    Add new exercises on Brazillian Summer School

---
+++ Brazillian_Summer_School_2026/week1/n.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tipos básicos
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+// Constantes comuns
+const int INF = 1e9+7;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+// Direções para grafos/grelhas
+const int dx4[4] = {-1,0,1,0};
+const int dy4[4] = {0,1,0,-1};
+
+const int dx8[8] = {-1,-1,-1,0,0,1,1,1};
+const int dy8[8] = {-1,0,1,-1,1,-1,0,1};
+
+// Macros rápidos
+#define all(x) (x).begin(),(x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define f first
+#define s second
+#define endl '\n'
+
+// Fast I/O
+struct IO {
+	IO() {
+		ios::sync_with_stdio(false);
+		cin.tie(nullptr);
+	}
+} io;
+
+
+void solution(){
+	ll a, b, c;
+	cin >> a >> b >> c;
+	if (a==0 && b==0 && c==0)
+		return;
+	while (a!=0 || b!=0 || c!=0) {
+		if (b-a == c-b) cout << "AP " << c+(c-b) << endl;
+		else cout << "GP " << c*(c/b) << endl;
+		cin >> a >> b >> c;
+	}
+}
+
+int main() {
+    IO io;
+	solution();
+	return 0;
+}
+
+
+
+
+
+
+
+
+
