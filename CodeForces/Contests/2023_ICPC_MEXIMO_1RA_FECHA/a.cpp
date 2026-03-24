67778ad73f8b9a71c94dec24803ed5a79026f878
Antonio-RF <antoniidaressurreicao@gmail.com>
2026-03-24 08:08:23 -0300

    Add new exercises

---
+++ CodeForces/Contests/2023_ICPC_MEXIMO_1RA_FECHA/a.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Tipos básicos
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+ 
+// Constantes comuns
+const int INF = 1e9+7;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+ 
+// Direções para grafos/grelhas
+const int dx4[4] = {-1,0,1,0};
+const int dy4[4] = {0,1,0,-1};
+ 
+const int dx8[8] = {-1,-1,-1,0,0,1,1,1};
+const int dy8[8] = {-1,0,1,-1,1,-1,0,1};
+ 
+// Macros rápidos
+#define all(x) (x).begin(),(x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define f first
+#define s second
+#define endl '\n'
+ 
+// Fast I/O
+struct IO {
+	IO() {
+		ios::sync_with_stdio(false);
+		cin.tie(nullptr);
+	}
+} io;
+ 
+ 
+void solution(){
+	ll t;
+	cin >> t;
+	map<string,ll> v;
+	ll ans = 0;
+	for (int i=0 ; i<t ; i++) {
+		ll n;
+		cin >> n;
+		string temp = "";
+		for (int i=0 ; i<n ; i++) {
+			string a;
+			cin >> a;
+			temp += a[0];
+		}
+		if (v[temp]==-1) continue;
+		else if (v[temp]>0) {
+			v[temp] = -1;
+			ans--;
+			continue;
+		}
+		else {
+			v[temp]++;
+			ans++;
+		}
+	}
+
+	cout << ans << endl;
+
+}
+ 
+int main() {
+    IO io;
+	solution();
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
